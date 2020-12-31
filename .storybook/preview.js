f57666d9b0abe084b943013a40c00507046e99c3
caioaugustoo <caioamfr@gmail.com>
2020-12-31 02:44:17 -0300

    Import do theme e uso do ThemeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,17 @@
 import { addDecorator } from '@storybook/react'
 import { withNextRouter } from 'storybook-addon-next-router'
-import GlobalStyles from '../src/styles/global'
+import GlobalStyles from 'styles/global'
+
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
 
 addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]

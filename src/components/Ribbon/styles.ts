08cba93c995a97b10180a3a82fa272037e8ebb21
caioaugustoo <caioamfr@gmail.com>
2021-02-20 20:20:55 -0300

    Criado estilos do Ribbon

--- src/components/Ribbon/styles.ts
+++ src/components/Ribbon/styles.ts
@@ -1,23 +1,62 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 
 import { RibbonColors, RibbonProps } from '.'
+import { darken } from 'polished'
 
 const wrapperModifiers = {
   color: (theme: DefaultTheme, color: RibbonColors) => css`
     background-color: ${theme.colors[color]};
+
+    &::before {
+      border-left-color: ${darken(0.2, theme.colors[color])};
+      border-top-color: ${darken(0.2, theme.colors[color])};
+    }
   `,
   normal: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.small};
     height: 3.6rem;
+    padding: 0 ${theme.spacings.small};
+    right: -2rem;
+
+    &::before {
+      top: 3.6rem;
+      border-top-width: 1rem;
+      border-right-width: 2rem;
+    }
   `,
   small: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.xsmall};
     height: 2.6rem;
+    padding: 0 ${theme.spacings.xsmall};
+
+    &::before {
+      top: 2.6rem;
+      border-top-width: 0.7rem;
+      border-right-width: 1.5rem;
+    }
   `
 }
 
 export const Wrapper = styled.div<Omit<RibbonProps, 'children'>>`
   ${({ theme, color, size }) => css`
+    position: absolute;
+    top: ${theme.spacings.xsmall};
+    display: flex;
+    align-items: center;
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
+
+    &::before {
+      content: '';
+      position: absolute;
+      right: 0;
+      border-style: solid;
+      border-left-width: 0rem;
+      border-right-color: transparent;
+      border-bottom-color: transparent;
+      border-bottom-width: 1rem;
+    }
+
     ${!!color && wrapperModifiers.color(theme, color)};
     ${!!size && wrapperModifiers[size](theme)};
   `}

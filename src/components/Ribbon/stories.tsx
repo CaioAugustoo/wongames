43f9b345cac65fc5e12f7f817b0b22d403252fa8
caioaugustoo <caioamfr@gmail.com>
2021-02-20 15:50:28 -0300

    Criada estrutura do Ribbon com TDD

---
+++ src/components/Ribbon/stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Ribbon, { RibbonProps } from '.'
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon,
+  args: {
+    children: 'Best Seller'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: Story<RibbonProps> = (args) => <Ribbon {...args} />

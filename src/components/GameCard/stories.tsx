21a9ee70571341e75693ee21e5083719ea414e37
caioaugustoo <caioamfr@gmail.com>
2021-02-28 13:48:51 -0300

    Criado estilos do GamesCard

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -8,12 +8,18 @@ export default {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 'R$ 235,00'
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
   }
 } as Meta
 
 export const Default: Story<GameCardProps> = (args) => (
-  <div style={{ width: '130rem' }}>
+  <div style={{ maxWidth: '29rem' }}>
     <GameCard {...args} />
   </div>
 )

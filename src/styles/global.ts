a847184c0b7ae90b8587d5f015e925471fa3f402
caioaugustoo <caioamfr@gmail.com>
2021-03-12 10:49:41 -0300

    Ajustes nos estilos globais

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,6 +31,11 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    ::after {
+      box-sizing: inherit;
+    }
   }
 
   ${({ theme }) => css`
@@ -47,6 +52,7 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+      background-color: ${theme.colors.mainBg};
     }
   `}
 `

5f69a521f2efc058f77f14263ca178a4a8ff6aba
caioaugustoo <caioamfr@gmail.com>
2021-03-12 14:46:41 -0300

    Otimização dos testes da home

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighLightProps } from '.'
 
+import item from './mock'
+
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Red Dead is back',
-    subtitle: "Come see John's new adventures",
-    buttonLabel: 'Buy now',
-    buttonLink: '/rdr2',
-    backgroundImage: '/img/red-dead-img.jpg'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighLightProps> = (args) => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
 
-export default function Index() {
-  return <Home />
+import bannersMock from 'components/BannerSlide/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import hightLightMock from 'components/Highlight/mock'
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: hightLightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: hightLightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: hightLightMock
+    }
+  }
 }

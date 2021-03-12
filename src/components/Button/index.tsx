b4bf35f674ceb9696c9a8ac7f56b3ba4fb969e18
caioaugustoo <caioamfr@gmail.com>
2021-03-12 18:25:16 -0300

    Criada variável minimal para o button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,6 +8,7 @@ type ButtonTypes =
 export type ButtonProps = {
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  minimal?: boolean
   icon?: JSX.Element
   as?: React.ElementType
 } & ButtonTypes
@@ -17,9 +18,16 @@ const Button = ({
   size = 'medium',
   fullWidth = false,
   icon,
+  minimal = false,
   ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    minimal={minimal}
+    {...props}
+  >
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,10 +1,11 @@
 import styled, { css, DefaultTheme } from 'styled-components'
+import { darken } from 'polished'
 
 import { ButtonProps } from '.'
 
 type WrapperProps = {
   hasIcon: boolean
-} & Pick<ButtonProps, 'size' | 'fullWidth'>
+} & Pick<ButtonProps, 'size' | 'fullWidth' | 'minimal'>
 
 const WrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -24,6 +25,14 @@ const WrapperModifiers = {
   fullWidth: () => css`
     width: 100%;
   `,
+  minimal: (theme: DefaultTheme) => css`
+    background: none;
+    color: ${theme.colors.primary};
+
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
+    }
+  `,
   withIcon: (theme: DefaultTheme) => css`
     svg {
       width: 1.5rem;
@@ -36,7 +45,7 @@ const WrapperModifiers = {
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: none;
@@ -50,11 +59,14 @@ export const Wrapper = styled.button<WrapperProps>`
     justify-content: center;
 
     &:hover {
-      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+      background: ${minimal
+        ? 'none'
+        : 'linear-gradient(180deg, #e35565 0%, #d958a6 50%)'};
     }
 
     ${!!size && WrapperModifiers[size](theme)}
     ${!!fullWidth && WrapperModifiers.fullWidth()}
     ${!!hasIcon && WrapperModifiers.withIcon(theme)}
+    ${!!minimal && WrapperModifiers.minimal(theme)}
   `}
 `

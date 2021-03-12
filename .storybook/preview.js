8e7526f4b2690c7e14af7f15943fc9c1bc5e135d
caioaugustoo <caioamfr@gmail.com>
2021-03-12 11:22:22 -0300

    Criado backgrounds customizados para o Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,6 +7,22 @@ import theme from 'styles/theme'
 
 addDecorator(withNextRouter())
 
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>

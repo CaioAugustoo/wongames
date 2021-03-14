e4a9bf873ce771580d4c9cc2284fb1a2f72bab9d
caioaugustoo <caioamfr@gmail.com>
2021-03-14 14:39:17 -0300

    Criado componente de Radio Button

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -6,11 +6,15 @@ import Checkbox from '.'
 
 describe('<Checkbox />', () => {
   it('should render with label', () => {
-    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+    const { container } = renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" />
+    )
 
     expect(screen.getByRole('checkbox')).toBeInTheDocument()
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render without label', () => {

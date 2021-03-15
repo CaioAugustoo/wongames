bbff32ad44b481a062e6857bc13b026b7cf156bd
caioaugustoo <caioamfr@gmail.com>
2021-03-15 07:52:52 -0300

    Agrupando components de form no storybook

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -3,7 +3,7 @@ import TextField, { TextFieldProps } from '.'
 import { Email } from '@styled-icons/material-outlined'
 
 export default {
-  title: 'TextField',
+  title: 'Form/TextField',
   component: TextField,
   args: {
     label: 'E-mail',

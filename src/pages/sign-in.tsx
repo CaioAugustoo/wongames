3081ecc983a70f35813ae2cfb77a29ea7c3a75aa
caioaugustoo <caioamfr@gmail.com>
2021-03-15 10:46:32 -0300

    Conectando os components nas páginas de signin e signup

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,7 +1,12 @@
+import FormSignIn from 'components/FormSignIn'
 import Auth from '../templates/Auth'
 
 const SignIn = () => {
-  return <Auth title="Sign In">children</Auth>
+  return (
+    <Auth title="Sign In">
+      <FormSignIn />
+    </Auth>
+  )
 }
 
 export default SignIn
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,7 +1,12 @@
+import FormSignUp from 'components/FormSignUp'
 import Auth from '../templates/Auth'
 
 const SignUp = () => {
-  return <Auth title="Sign Up">children</Auth>
+  return (
+    <Auth title="Sign Up">
+      <FormSignUp />
+    </Auth>
+  )
 }
 
 export default SignUp

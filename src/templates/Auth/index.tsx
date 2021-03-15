ceaae510ba3cf6273de9365b685f6ce1174069f7
caioaugustoo <caioamfr@gmail.com>
2021-03-15 09:07:30 -0300

    Criado estilo do template Auth para desktop

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -13,26 +13,35 @@ const Auth = ({ title, children }: AuthProps) => {
   return (
     <S.Wrapper>
       <S.BannerBlock>
-        <Logo />
+        <S.BannerContent>
+          <Logo />
 
-        <Heading color="white">All your favorite games in one place</Heading>
+          <div>
+            <Heading color="white">
+              All your favorite games in one place
+            </Heading>
 
-        <S.Subtitle>
-          <strong>WON</strong> is the best and most complete gaming plataform.
-        </S.Subtitle>
+            <S.Subtitle>
+              <strong>WON</strong> is the best and most complete gaming
+              plataform.
+            </S.Subtitle>
+          </div>
 
-        <S.Footer>
-          Won Games {getCurrentYear} © Todos os Direitos Reservados
-        </S.Footer>
+          <S.Footer>
+            Won Games {getCurrentYear} © Todos os Direitos Reservados
+          </S.Footer>
+        </S.BannerContent>
       </S.BannerBlock>
 
       <S.Content>
-        <Logo color="black" size="large" />
-        <Heading color="black" lineColor="secondary" lineLeft>
-          {title}
-        </Heading>
+        <S.ContentWrapper>
+          <Logo color="black" size="large" />
+          <Heading color="black" lineColor="secondary" lineLeft>
+            {title}
+          </Heading>
 
-        {children}
+          {children}
+        </S.ContentWrapper>
       </S.Content>
     </S.Wrapper>
   )
--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,11 +1,84 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import * as HeadingStyles from 'components/Heading/styles'
+import * as LogoStyles from 'components/Logo/styles'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+`
 
-export const BannerBlock = styled.div``
+export const BannerBlock = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    background-image: url(/img/auth-bg.jpg);
+    background-size: cover;
+    background-position: center center;
+    padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
+      ${theme.spacings.large};
 
-export const Subtitle = styled.h3``
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: ${theme.colors.black};
+      opacity: 0.85;
+    }
+  `}
+`
 
-export const Footer = styled.p``
+export const BannerContent = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 1fr;
+    height: 100%;
+    justify-content: space-between;
+    position: relative;
+    z-index: ${theme.layers.base};
+    color: ${theme.colors.white};
+  `}
+`
 
-export const Content = styled.div``
+export const Subtitle = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+    font-weight: ${theme.font.light};
+    margin-top: ${theme.spacings.xxsmall};
+    strong {
+      color: ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Footer = styled.p`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xsmall};
+    text-align: center;
+    align-self: end;
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+    display: grid;
+    align-items: center;
+    justify-content: center;
+  `}
+`
+
+export const ContentWrapper = styled.div`
+  ${({ theme }) => css`
+    width: 30rem;
+
+    ${LogoStyles.Wrapper} {
+      margin: 0 auto ${theme.spacings.xxlarge};
+    }
+    ${HeadingStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+  `}
+`

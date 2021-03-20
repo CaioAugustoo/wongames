e7a7443b322d5d9bdc1f4fe8219cd52c8328124e
caioaugustoo <caioamfr@gmail.com>
2021-03-20 20:37:27 -0300

    Criada estrutura de showcase

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,85 +4,91 @@ import Heading from '../Heading/index'
 import Logo from '../Logo/index'
 import * as S from './styles'
 
-const Footer = () => (
-  <S.Wrapper>
-    <Logo color="black" />
-    <S.Content>
-      <S.Column>
-        <Heading color="black" size="small" lineBottom lineColor="secondary">
-          Contact Us
-        </Heading>
+const Footer = () => {
+  const getCurrentYear = new Date().getFullYear()
 
-        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
-      </S.Column>
+  return (
+    <S.Wrapper>
+      <Logo color="black" />
+      <S.Content>
+        <S.Column>
+          <Heading color="black" size="small" lineBottom lineColor="secondary">
+            Contact Us
+          </Heading>
 
-      <S.Column>
-        <Heading color="black" lineColor="secondary" lineBottom size="small">
-          Follow us
-        </Heading>
+          <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+        </S.Column>
 
-        <nav aria-labelledby="social media">
-          <a
-            href="https://www.instagram.com/won-games"
-            target="_blank"
-            rel="noopenner, noreferrer"
-          >
-            Instagram
-          </a>
-          <a
-            href="https://www.twitter.com/won-games"
-            target="_blank"
-            rel="noopenner, noreferrer"
-          >
-            Twitter
-          </a>
-          <a
-            href="https://www.youtube.com/won-games"
-            target="_blank"
-            rel="noopenner, noreferrer"
-          >
-            Youtube
-          </a>
-          <a
-            href="https://www.facebook.com/won-games"
-            target="_blank"
-            rel="noopenner, noreferrer"
-          >
-            Facebook
-          </a>
-        </nav>
-      </S.Column>
+        <S.Column>
+          <Heading color="black" lineColor="secondary" lineBottom size="small">
+            Follow us
+          </Heading>
 
-      <S.Column>
-        <Heading color="black" lineColor="secondary" lineBottom size="small">
-          Links
-        </Heading>
+          <nav aria-labelledby="social media">
+            <a
+              href="https://www.instagram.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.twitter.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://www.youtube.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Youtube
+            </a>
+            <a
+              href="https://www.facebook.com/won-games"
+              target="_blank"
+              rel="noopenner, noreferrer"
+            >
+              Facebook
+            </a>
+          </nav>
+        </S.Column>
 
-        <nav aria-labelledby="footer resources">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/games">
-            <a>Store</a>
-          </Link>
-          <Link href="/search">
-            <a>Buscar</a>
-          </Link>
-        </nav>
-      </S.Column>
+        <S.Column>
+          <Heading color="black" lineColor="secondary" lineBottom size="small">
+            Links
+          </Heading>
 
-      <S.Column aria-labelledby="footer-contact">
-        <Heading color="black" lineColor="secondary" lineBottom size="small">
-          Location
-        </Heading>
-        <span>Lorem ipsum dolor sit.</span>
-        <span>Lorem Ipsum</span>
-        <span>Lorem, ipsum dolor.</span>
-      </S.Column>
-    </S.Content>
+          <nav aria-labelledby="footer resources">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <Link href="/games">
+              <a>Store</a>
+            </Link>
+            <Link href="/search">
+              <a>Buscar</a>
+            </Link>
+          </nav>
+        </S.Column>
 
-    <S.Copyright>Won Games 2020 © All rights reserved.</S.Copyright>
-  </S.Wrapper>
-)
+        <S.Column aria-labelledby="footer-contact">
+          <Heading color="black" lineColor="secondary" lineBottom size="small">
+            Location
+          </Heading>
+          <span>Lorem ipsum dolor sit.</span>
+          <span>Lorem Ipsum</span>
+          <span>Lorem, ipsum dolor.</span>
+        </S.Column>
+      </S.Content>
+
+      <S.Copyright>
+        Won Games {getCurrentYear} © All rights reserved.
+      </S.Copyright>
+    </S.Wrapper>
+  )
+}
 
 export default Footer
---
+++ src/components/Showcase/styles.ts
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components'
+
+import media from 'styled-media-query'
+
+import * as HeadingStyles from 'components/Heading/styles'
+import * as GameCardSliderStyles from 'components/GameCardSlider/styles'
+import * as HighlightStyles from 'components/Highlight/styles'
+
+import { Container } from 'components/Container'
+
+export const Wrapper = styled(Container).attrs({ as: 'section' })`
+  ${({ theme }) => css`
+    ${HeadingStyles.Wrapper},
+    ${HighlightStyles.Wrapper},
+    ${GameCardSliderStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+    ${HighlightStyles.Wrapper} {
+      ${media.lessThan('medium')`
+        margin-right: calc(-${theme.grid.gutter} / 2);
+        margin-left: calc(-${theme.grid.gutter} / 2);
+      `}
+    }
+    ${GameCardSliderStyles.Wrapper} {
+      ${media.lessThan('huge')`
+        margin-right: calc(-${theme.grid.gutter} / 2);
+      `}
+    }
+    margin-bottom: calc(${theme.spacings.large} * 2);
+  `}
+`

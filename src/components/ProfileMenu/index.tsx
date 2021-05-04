17ea0cdc1830d2b4a33a186b0f1efbe1b28fff10
Caio Augusto <caioamfr@gmail.com>
2021-05-04 13:52:39 -0300

    Adicionado estilo do link ativo

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -6,38 +6,44 @@ import {
   FormatListBulleted
 } from '@styled-icons/material-outlined'
 
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' 
+}
+
 import * as S from './styles'
 
-const ProfileMenu = () => (
-  <S.Nav>
-    <Link href="/profile/me" passHref>
-      <S.Link>
-        <AccountCircle size={24} />
-        <span>Meu perfil</span>
-      </S.Link>
-    </Link>
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => {
+  return (
+    <S.Nav>
+      <Link href="/profile/me" passHref>
+        <S.Link isActive={activeLink === '/profile/me'} title="Meu Perfil">
+          <AccountCircle size={24} />
+          <span>Meu perfil</span>
+        </S.Link>
+      </Link>
 
-    <Link href="/profile/cards" passHref>
-      <S.Link>
-        <CreditCard size={24} />
-        <span>Meus cartões</span>
-      </S.Link>
-    </Link>
+      <Link href="/profile/cards" passHref>
+        <S.Link isActive={activeLink === '/profile/cards'} title="Meus cartões">
+          <CreditCard size={24} />
+          <span>Meus cartões</span>
+        </S.Link>
+      </Link>
 
-    <Link href="/profile/orders" passHref>
-      <S.Link>
-        <FormatListBulleted size={24} />
-        <span>Minhas compras</span>
-      </S.Link>
-    </Link>
+      <Link href="/profile/orders" passHref>
+        <S.Link isActive={activeLink === '/profile/orders'} title="Minhas compras">
+          <FormatListBulleted size={24} />
+          <span>Minhas compras</span>
+        </S.Link>
+      </Link>
 
-    <Link href="/logout" passHref>
-      <S.Link>
-        <ExitToApp size={24} />
-        <span>Sair</span>
-      </S.Link>
-    </Link>
-  </S.Nav>
-)
+      <Link href="/logout" passHref>
+        <S.Link>
+          <ExitToApp size={24} />
+          <span>Sair</span>
+        </S.Link>
+      </Link>
+    </S.Nav>
+  )
+}
 
 export default ProfileMenu

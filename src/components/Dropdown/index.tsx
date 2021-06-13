11d533ed3f6d188cf610e558afc89b8f279f59f2
caioaugustoo <caioamfr@gmail.com>
2021-06-13 18:15:44 -0300

    Criado tests para o dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import * as S from './styles'
 
 export type DropdownProps = {
@@ -5,11 +6,15 @@ export type DropdownProps = {
   children: React.ReactNode
 }
 
-const Dropdown = ({ title, children }: DropdownProps) => (
-  <S.Wrapper>
-    <S.Title>{title}</S.Title>
-    <S.Content>{children}</S.Content>
-  </S.Wrapper>
-)
+const Dropdown = ({ title, children }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper isOpen={isOpen}>
+      <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
+      <S.Content aria-hidden={!isOpen}>{children}</S.Content>
+    </S.Wrapper>
+  )
+}
 
 export default Dropdown

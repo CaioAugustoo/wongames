67c5b93b99cf50e7c28727a64c24b62aba4aa0c5
caioaugustoo <caioamfr@gmail.com>
2021-06-13 19:12:38 -0300

    Stories pra dropdown sem items

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -6,14 +6,14 @@ import * as S from './styles'
 import CartList from 'components/CartList'
 
 export type CartDropdownProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
 }
 
 const CartDropdown = ({ items, total }: CartDropdownProps) => (
   <S.Wrapper>
-    <Dropdown title={<CartIcon quantity={items.length} />}>
-      <CartList items={items} total={total} hasButton />
+    <Dropdown title={<CartIcon quantity={items?.length ?? 0} />}>
+      <CartList items={items!} total={total!} hasButton />
     </Dropdown>
   </S.Wrapper>
 )

00ff9807f3e1cf3cfbb1eba181a8a139a5f94b1b
Caio Augusto <caioamfr@gmail.com>
2021-06-15 14:18:54 -0300

    Adicionado seção de recomendados no carrinho de compras

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,6 +3,10 @@ import Cart, { CartProps } from 'templates/Cart'
 import gamesMock from 'components/GameCardSlider/mock'
 import cardsMock from 'components/PaymentOptions/mock'
 import highLightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 const CartPage = (props: CartProps) => {
   return <Cart {...props} />
@@ -11,11 +15,18 @@ const CartPage = (props: CartProps) => {
 export default CartPage
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
       games: gamesMock,
-      recommendedGames: gamesMock.slice(0, 5),
-      recommendedHighLight: highLightMock,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighLight: highlightMapper(
+        data.recommended?.section?.highlight
+      ),
       items: gamesMock.slice(0, 2),
       total: 'R$ 470,00',
       cards: cardsMock

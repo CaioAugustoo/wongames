7ee36f00b599cdfcd755c8274587dd2d06de0346
Caio Augusto <caioamfr@gmail.com>
2021-07-01 15:07:16 -0300

    Adicionando um Overlay no Dropdown para fechar ao clicar fora

--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -22,7 +22,7 @@ export const Wrapper = styled.div<WrapperProps>`
     position: relative;
     width: max-content;
 
-    ${Content} {
+    ${Content}, ${Overlay} {
       transition: transform 0.2s ease-in, opacity ${theme.transition.default};
       ${isOpen && wrapperModifiers.open()}
       ${!isOpen && wrapperModifiers.close()}
@@ -38,6 +38,7 @@ export const Title = styled.div`
     display: flex;
     align-items: center;
     padding-right: 2.4rem;
+    z-index: ${theme.layers.alwaysOnTop};
   `}
 `
 
@@ -50,6 +51,7 @@ export const Content = styled.div`
     margin-top: ${theme.spacings.small};
     position: absolute;
     right: 0;
+    z-index: ${theme.layers.alwaysOnTop};
 
     &::before {
       content: '';
@@ -60,5 +62,18 @@ export const Content = styled.div`
       top: -1.2rem;
       right: 2.4rem;
     }
+  `}
+`
+
+export const Overlay = styled.div`
+  ${({ theme }) => css`
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: ${theme.layers.overlay};
+    cursor: initial;
   `}
 `

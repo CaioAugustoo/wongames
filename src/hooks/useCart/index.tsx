e3720f4754240ff399e7cf20ddb7f818612829fd
caioaugustoo <caioamfr@gmail.com>
2021-07-24 17:36:40 -0300

    Usando MockedProvider para tests

--- src/hooks/useCart/index.tsx
+++ src/hooks/useCart/index.tsx
@@ -1,10 +1,19 @@
+import { useQueryGames } from 'graphql/queries/games'
 import { useContext, createContext, useState, useEffect } from 'react'
+import { formatPrice } from 'utils/formatters/price'
 import { getStorageItem } from 'utils/localStorage'
 
 const CART_KEY = 'cartItems'
 
+export type CartItem = {
+  id: string
+  img: string
+  title: string
+  price: string
+}
+
 export type CartContextData = {
-  items: string[]
+  items: CartItem[]
 }
 
 export type CartProviderProps = {
@@ -22,8 +31,26 @@ const CartProvider = ({ children }: CartProviderProps) => {
     if (data) setCartItems(data)
   }, [])
 
+  const { data } = useQueryGames({
+    skip: !cartItems?.length,
+    variables: {
+      where: {
+        id: cartItems
+      }
+    }
+  })
+
   return (
-    <CartContext.Provider value={{ items: cartItems }}>
+    <CartContext.Provider
+      value={{
+        items: data?.games.map((game) => ({
+          id: game.id,
+          img: game.cover?.url,
+          price: formatPrice(game.price),
+          title: game.name
+        }))
+      }}
+    >
       {children}
     </CartContext.Provider>
   )

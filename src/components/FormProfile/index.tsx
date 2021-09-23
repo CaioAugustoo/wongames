43218902d081dfba8c129d6fdc724d429ed68a0b
caioaugustoo <caioamfr@gmail.com>
2021-09-22 22:01:06 -0300

    Preenchimento de dados através da session

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -1,41 +1,59 @@
 import Heading from 'components/Heading'
 import TextField from 'components/TextField'
 import Button from 'components/Button'
+import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
+
+import { useSession } from 'next-auth/client'
 
 import * as S from './styles'
 
-const FormProfile = () => (
-  <S.Wrapper>
-    <Heading size="small" color="black" lineBottom lineColor="primary">
-      My profile
-    </Heading>
+const FormProfile = () => {
+  const [session] = useSession()
 
-    <S.Form>
-      <TextField name="name" placeholder="Name" label="Name" />
-      <TextField
-        name="email"
-        type="email"
-        placeholder="Email"
-        label="Email"
-        initialValue="caioamfr@gmail.com"
-        disabled
-      />
-      <TextField
-        type="password"
-        name="password"
-        placeholder="Your Password"
-        label="Password"
-      />
-      <TextField
-        type="password"
-        name="new_password"
-        placeholder="New Password"
-        label="New Password"
-      />
+  return (
+    <S.Wrapper>
+      <Heading size="small" color="black" lineBottom lineColor="primary">
+        My profile
+      </Heading>
 
-      <Button size="large">Save</Button>
-    </S.Form>
-  </S.Wrapper>
-)
+      <S.Form>
+        <TextField
+          icon={<AccountCircle />}
+          name="name"
+          placeholder="Name"
+          label="Name"
+          value={session?.user?.name ?? ''}
+          disabled
+        />
+        <TextField
+          icon={<Email />}
+          name="email"
+          type="email"
+          placeholder="Email"
+          label="Email"
+          value={session?.user?.email ?? ''}
+          disabled
+        />
+        <TextField
+          icon={<Lock />}
+          type="password"
+          name="password"
+          placeholder="Your Password"
+          label="Password"
+          value=""
+        />
+        <TextField
+          icon={<Lock />}
+          type="password"
+          name="new_password"
+          placeholder="New Password"
+          label="New Password"
+        />
+
+        <Button size="large">Save</Button>
+      </S.Form>
+    </S.Wrapper>
+  )
+}
 
 export default FormProfile

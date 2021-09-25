10c538b91ff55c6abc52ebe53dce4f7648091293
caioaugustoo <caioamfr@gmail.com>
2021-09-25 13:26:48 -0300

    Configurando o Apollo HTTP Link para chamadas autenticadas

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,19 +1,38 @@
 import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { useMemo } from 'react'
 import apolloCache from './apolloCache'
 
 let apolloClient: ApolloClient<NormalizedCacheObject | null>
 
-function createApolloClient() {
+// Todo: Find correct type
+type Session = {
+  jwt: string
+}
+
+function createApolloClient(session: Session | null) {
+  const httpLink = new HttpLink({
+    uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    const authorization = session?.jwt ? `Bearer ${session.jwt}` : ''
+
+    return { headers: { ...headers, authorization } }
+  })
+
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    link: authLink.concat(httpLink),
     cache: apolloCache
   })
 }
 
-export function initializeApollo(initialState = null) {
-  const apolloClientGlobal = apolloClient ?? createApolloClient()
+export function initializeApollo(
+  initialState = null,
+  session?: Session | null
+) {
+  const apolloClientGlobal = apolloClient ?? createApolloClient(session!)
 
   if (initialState) {
     apolloClientGlobal.cache.restore(initialState)
@@ -25,7 +44,10 @@ export function initializeApollo(initialState = null) {
   return apolloClient
 }
 
-export function useApollo(initialState = null) {
-  const store = useMemo(() => initializeApollo(initialState), [initialState])
+export function useApollo(initialState = null, session?: Session) {
+  const store = useMemo(
+    () => initializeApollo(initialState, session),
+    [initialState, session]
+  )
   return store
 }

fd0c7cd11965bba1efe9cb4609a440b19857f33e
caioaugustoo <caioamfr@gmail.com>
2021-10-14 17:44:09 -0300

    Criando handler do MSW para o ForgotPassword para interceptar chamadas

--- src/utils/mockSever/handlers.ts
+++ src/utils/mockSever/handlers.ts
@@ -1,3 +1,34 @@
 import { rest } from 'msw'
 
-export const handlers = []
+type LoginReqBody = {
+  email: string
+}
+
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body
+      if (email === 'false@email.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [{ message: 'This email does not exist' }]
+              }
+            ]
+          })
+        )
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true
+        })
+      )
+    }
+  )
+]

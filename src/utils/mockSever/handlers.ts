35f51af319fcc2fcaaae37b927359ae83aa6565e
caioaugustoo <caioamfr@gmail.com>
2021-12-07 12:40:16 -0300

    Handler para reset de senha

--- src/utils/mockSever/handlers.ts
+++ src/utils/mockSever/handlers.ts
@@ -4,6 +4,12 @@ type LoginReqBody = {
   email: string
 }
 
+type ResetReqBody = {
+  code: string
+  password: string
+  passwordConfirmation: string
+}
+
 export const handlers = [
   rest.post<LoginReqBody>(
     `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
@@ -27,6 +33,39 @@ export const handlers = [
         ctx.status(200),
         ctx.json({
           ok: true
+        })
+      )
+    }
+  ),
+  rest.post<ResetReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+    (req, res, ctx) => {
+      const { code } = req.body
+
+      if (code === 'wrong_code') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'Incorrect code provided'
+                  }
+                ]
+              }
+            ]
+          })
+        )
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          user: {
+            email: 'valid@email.com'
+          }
         })
       )
     }

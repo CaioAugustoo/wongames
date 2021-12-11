4d11cd043435a58f7a3ac3d499a1cbe39bb0cb8d
caioaugustoo <caioamfr@gmail.com>
2021-12-11 18:45:41 -0300

    Configurando o NextSEO default

---
+++ next-seo.config.js
@@ -0,0 +1,9 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://wongames.caioaugusto.dev',
+    site_name: 'Won Games',
+    title: 'Won Games'
+  }
+}

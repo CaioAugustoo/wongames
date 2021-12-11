8694712d996005f5e391733f3b5ee72b1f195618
caioaugustoo <caioamfr@gmail.com>
2021-12-11 18:22:42 -0300

    Atualização de pacotes e tipos

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next-auth/internals/utils'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 

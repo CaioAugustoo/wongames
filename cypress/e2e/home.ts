5f6d5d5f42f856c46eae770241609e61b3810e96
caioaugustoo <caioamfr@gmail.com>
2021-12-12 18:57:10 -0300

    Comando para showcase

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,5 +4,21 @@ describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/')
     cy.shouldRenderBanner()
+
+    cy.shouldRenderShowcase({
+      name: "News"
+    })
+
+    cy.shouldRenderShowcase({
+      name: "Most popular"
+    })
+
+    cy.shouldRenderShowcase({
+      name: "Recommended for you"
+    })
+
+    cy.shouldRenderShowcase({
+      name: "Free to play"
+    })
   })
 })

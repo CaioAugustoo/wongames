c9bbb5b900b825ac5798bcc5fae8bcdd6178af0c
caioaugustoo <caioamfr@gmail.com>
2021-12-12 20:50:34 -0300

    Adicionando o jogo no carrinho através da página de jogo

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,12 +1,14 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Game Page', () => {
-  it('should render game sections', () => {
+  before(() => {
     cy.visit('/game/cyberpunk-2077')
 
     // Wait page load
     cy.wait(4500)
+  })
 
+  it('should render game sections', () => {
     cy.getByDataCy("game-info").within(() => {
       cy.findByRole("heading", { name: "Cyberpunk 2077" }).should("exist")
       cy.findByText(/^Cyberpunk 2077 is an open-world, action-adventure story set in Night Cit/i)
@@ -50,5 +52,21 @@ describe('Game Page', () => {
     cy.shouldRenderShowcase({
       name: "Recommended for you"
     })
+  })
+
+  it("should add/remove game in cart", () => {
+    cy.getByDataCy("game-info").within(() => {
+      cy.findByLabelText("Add to cart").click()
+      cy.findByLabelText("Remove from cart").should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i).first().should("have.text", 1)
+
+    cy.getByDataCy("game-info").within(() => {
+      cy.findByLabelText("Remove from cart").click()
+      cy.findByLabelText("Add to cart").should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i).should("not.exist")
   })
 })

509a228967fe1522419017c0151db1aeba8d000a
caioaugustoo <caioamfr@gmail.com>
2021-12-14 00:02:57 -0300

    Criado teste de cadastro

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -1,14 +1,11 @@
 /// <reference path="../support/index.d.ts" />
 
+
+
 describe("User",() => {
   it("should signup", () => {
     cy.visit("/sign-up")
 
-    cy.findByRole("heading", { name: /Sign Up/i }).should("exist")
-    cy.findByPlaceholderText(/User Name/i).should("exist").type("cypress")
-    cy.findByPlaceholderText(/Email/i).should("exist").type("e2e@gmail.com")
-    cy.findByPlaceholderText(/^Password/i).should("exist").type("cypress-password")
-    cy.findByPlaceholderText(/Confirm Password/i).should("exist").type("cypress-password")
-    cy.findByRole("button", { name: /Sign Up now!/i }).should("exist").click()
+    cy.shouldSignup()
   })
 })

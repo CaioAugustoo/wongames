6ac2da275a5abeb04d95e8ff495b9f80cb44e916
caioaugustoo <caioamfr@gmail.com>
2021-12-14 11:38:48 -0300

    Mostrando erro de código inválido

--- cypress/e2e/reset-password.ts
+++ cypress/e2e/reset-password.ts
@@ -11,4 +11,32 @@ describe('Forgot Password', () => {
 
     cy.findByText("confirm password does not match with password").should("exist")
   })
+
+  it("should show error if code is invalid", () => {
+    cy.intercept("POST", "**/auth/reset-password", res => {
+      res.reply({
+        status: 400,
+        body: {
+          error: "Bad Request",
+          message: [
+            {
+              messages: [
+                {
+                  message: 'Incorrect code provided.'
+                }
+              ]
+            }
+          ]
+        }
+      })
+    })
+
+    cy.visit("/reset-password?code=1234567")
+
+    cy.findByPlaceholderText(/^password/i).type("123456")
+    cy.findByPlaceholderText(/confirm password/i).type("123456")
+
+    cy.findByRole("button", { name: /Reset password/i }).should("exist").click()
+    cy.findByText("Incorrect code provided.").should("exist")
+  })
 })
